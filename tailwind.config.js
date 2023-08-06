568c872774bd2713bc03afb0224de1b42596397a
unknown <brandon_mark_cantos@dlsl.edu.ph>
2023-08-06 12:10:02 +0800

    header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'puti':'#ffffff',
+        'medyoblack':'#333333',
+        'darkberde':'#002E27',
+        'lightberde':'#00B14F',
+        'textberde':'#3B9D74',
+        'bebeblue':'#EAEFF2'
+      }
+    },
   },
   plugins: [],
 }
